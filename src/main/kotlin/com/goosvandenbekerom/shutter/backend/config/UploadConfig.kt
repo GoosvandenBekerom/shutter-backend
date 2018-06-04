62167755be2d9c0a24035e019556de6b312ee69d
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-04 12:56:52 +0200

    Added gif to allowed extensions

--- src/main/kotlin/com/goosvandenbekerom/shutter/backend/config/UploadConfig.kt
+++ src/main/kotlin/com/goosvandenbekerom/shutter/backend/config/UploadConfig.kt
@@ -3,5 +3,5 @@ package com.goosvandenbekerom.shutter.backend.config
 import org.springframework.http.MediaType
 
 object UploadConfig {
-    val allowedFileTypes = setOf(MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE)
+    val allowedFileTypes = setOf(MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE)
 }
